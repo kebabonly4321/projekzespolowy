647067402927817e24da67fb9201311cf93bd38d
hubson <162894692+kebabonly4321@users.noreply.github.com>
2026-05-10 09:34:47 +0200

    dodanie przykladowych cwiczen do bazy

---
+++ database/przykladowe_cwiczenia.sql
@@ -0,0 +1,42 @@
+INSERT INTO cwiczenia (name, muscle_group_id) VALUES
+-- KLATKA PIERSIOWA (1)
+('Wyciskanie sztangi leżąc', 1),
+('Wyciskanie hantli skos dodatni', 1),
+('Rozpiętki hantlami', 1),
+('Pompki klasyczne', 1),
+('Chest press', 1),
+
+-- PLECY (2)
+('Martwy ciąg', 2),
+('Podciąganie na drążku', 2),
+('Wiosłowanie sztangą', 2),
+('Ściąganie drążka wyciągu górnego', 2),
+('Wiosłowanie hantlem', 2),
+
+-- NOGI (3)
+('Przysiad ze sztangą', 3),
+('Hack squat', 3),
+('Wypychanie nóg na suwnicy', 3),
+('Prostowanie nóg na maszynie', 3),
+('Uginanie nóg leżąc', 3),
+
+-- BARKI (4)
+('Wyciskanie żołnierskie', 4),
+('Unoszenie hantli bokiem', 4),
+('Arnold press', 4),
+('Face pull', 4),
+('Odwrotne rozpiętki', 4),
+
+-- BICEPS (5)
+('Uginanie ramion ze sztangą', 5),
+('Uginanie hantli stojąc', 5),
+('Hammer curl', 5),
+('Modlitewnik', 5),
+('Uginanie na wyciągu', 5),
+
+-- TRICEPS (6)
+('Wyciskanie francuskie', 6),
+('Prostowanie ramion na wyciągu', 6),
+('Dipy', 6),
+('Prostowanie hantla zza głowy', 6),
+('Close grip bench press', 6),
